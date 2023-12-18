da3e4af6cffd10087d6e7b65b9633efdb461ab27
JimmiXxX <jimmix139@gmail.com>
2023-12-18 19:27:54 +0300

    Первый деплой на гит

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const contentForm = document.querySelector('.content_form')
+
+newMenu = () =>{
+    contentForm.style.display = 'none' ? contentForm.style.display = 'flex' : contentForm.style.display='none'
+}
